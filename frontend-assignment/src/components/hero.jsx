3036b86032dbc7f357c0ac8e7891ce844bbac058
tejashwini-Bk-tech <tejashwinikoli2005@gmail.com>
2026-06-18 00:35:00 +0530

    commit

--- frontend-assignment/src/components/hero.jsx
+++ frontend-assignment/src/components/hero.jsx
@@ -6,6 +6,9 @@ import ellipse257 from '../assets/hero/Ellipse 257.png';
 import ellipse258 from '../assets/hero/Ellipse 258.svg';
 import ellipse259 from '../assets/hero/Ellipse 259.png';
 import ellipse260 from '../assets/hero/Ellipse 260.svg';
+import bgVector from '../assets/Ellipse736.png'
+import bgVector1 from '../assets/Vector 2511.png'
+import bgVector2 from '../assets/Vector 2510.png'
 import './hero.css';
 
 function hero() {
@@ -13,27 +16,31 @@ function hero() {
         <>
             <section className="hero">
                 <div className="hero-container">
+                      <img src={bgVector} alt="" className="hero-vector" />
+                       <img src={bgVector1} alt="" className="hero-vector-1" />
+                        <img src={bgVector2} alt="" className="hero-vector-2" />
                     <div className="hero-content">
                         <h1 className="hero-title">
-                            The <span className="hl hl-underline">thinkers</span> and doers were <span className="hl hl-pink">changing</span> the <span className="hl hl-green">status</span> Quo with
+                            The <span className="hl hl-underline">thinkers</span> and <br></br>doers were ch<span className="hl hl-pink">anging</span><br/> the <span className="hl hl-green">status</span> Quo with
                         </h1>
                         <p className="hero-description">We are a team of strategists, designers communicators, researchers. Togeather, we belive that progress only happens when you refuse to play things safe.</p>
                     </div>
                     <div className="hero-images">
                         <div className="hero-image-group">
-                            <div className="hero-image-pair">
+                            <div className="hero-image-pair pair-1">
+                                 <img src={ellipse261} alt="team member" />
                                 <img src={ellipse262} alt="team member" />
-                                <img src={ellipse261} alt="team member" />
+                               
                             </div>
-                            <div className="hero-image-pair">
+                            <div className="hero-image-pair pair-2">
                                 <img src={ellipse255} alt="team member" />
                                 <img src={ellipse256} alt="team member" />
                             </div>
-                            <div className="hero-image-pair">
+                            <div className="hero-image-pair  pair-3">
                                 <img src={ellipse257} alt="team member" />
                                 <img src={ellipse258} alt="team member" />
                             </div>
-                            <div className="hero-image-pair">
+                            <div className="hero-image-pair  pair-4">
                                 <img src={ellipse259} alt="team member" />
                                 <img src={ellipse260} alt="team member" />
                             </div>
